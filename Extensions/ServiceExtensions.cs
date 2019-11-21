c34c52e27a5aed2623c303a81d9045d01ba27826
Piero Giusti <pierophp@gmail.com>
2019-11-21 17:43:56 -0300

    First Commit

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PinyinCardApi.Controllers
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+    }
+
+}
