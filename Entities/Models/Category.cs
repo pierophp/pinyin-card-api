7822b58c46bc1f714b4c3ae468f7bce716d68a27
Piero Giusti <pierophp@gmail.com>
2019-11-25 12:40:39 -0300

    Category

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -11,8 +11,20 @@ namespace Entities.Models
         public int Id { get; set; }
 
         [StringLength(255, ErrorMessage = "NameEn can't be longer than 255 characters")]
-        public string Name_En { get; set; }
+        [Column("name_en")]
+        public string NameEn { get; set; }
 
+        [StringLength(255, ErrorMessage = "NamePt can't be longer than 255 characters")]
+        [Column("name_pt")]
+        public string NamePt { get; set; }
+
+        [StringLength(255, ErrorMessage = "NameCht can't be longer than 255 characters")]
+        [Column("name_cht")]
+        public string NameCht { get; set; }
+
+        [StringLength(255, ErrorMessage = "NameChs can't be longer than 255 characters")]
+        [Column("name_chs")]
+        public string NameChS { get; set; }
 
     }
 }
