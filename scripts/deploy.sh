c7f404cce49f9e042632e32f81decca3b5e4c8f6
Piero Giusti <pierophp@gmail.com>
2020-01-23 12:10:45 -0300

    Folder SRC

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+dotnet publish
+zip -r dist.zip bin/Debug/netcoreapp3.0
+scp dist.zip pinyin:~
+ssh pinyin 'rm -Rf bin'
+ssh pinyin 'unzip dist.zip'
+ssh pinyin 'rm -Rf /var/www/pinyin-card-api/*'
+ssh pinyin 'cp -R bin/Debug/netcoreapp3.0/* /var/www/pinyin-card-api/'
+ssh pinyin 'sudo supervisorctl restart pinyin-card-api'
+
+
