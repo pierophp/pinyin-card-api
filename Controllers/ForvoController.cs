45c8105eeb8889c885e1346041f0083731cee177
Piero Giusti <pierophp@gmail.com>
2020-01-24 14:30:06 -0300

    Remove src folder

---
+++ Controllers/ForvoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PinyinCardApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ForvoController : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // async Task<
+            try
+            {
+                // var client = new HttpClient();
+                // var response = await client.GetAsync("https://forvo.com/word/duck/#en_usa");
+
+                return Ok(new
+                {
+                    welcome = "Welcome to Pinyin Card"
+                });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Something went wrong inside GetAll action: {ex.Message}");
+            }
+        }
+    }
+}
