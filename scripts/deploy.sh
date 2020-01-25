ed9e9ab0d35e9b337beb6d7e431084827168a0e0
Piero Giusti <pierophp@gmail.com>
2020-01-25 11:12:59 -0300

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,10 @@
 dotnet publish
-zip -r dist.zip bin/Debug/netcoreapp3.0
+zip -r dist.zip bin/Debug/netcoreapp3.0/publish
 scp dist.zip pinyin:~
 ssh pinyin 'rm -Rf bin'
 ssh pinyin 'unzip dist.zip'
 ssh pinyin 'rm -Rf /var/www/pinyin-card-api/*'
-ssh pinyin 'cp -R bin/Debug/netcoreapp3.0/* /var/www/pinyin-card-api/'
+ssh pinyin 'cp -R bin/Debug/netcoreapp3.0/publish/* /var/www/pinyin-card-api/'
 ssh pinyin 'sudo supervisorctl restart pinyin-card-api'
 
 
