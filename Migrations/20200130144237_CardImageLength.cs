3e913af8b0b29720fe641b502f4bbe63a0f29d92
Piero Giusti <pierophp@gmail.com>
2020-01-30 11:45:15 -0300

    Image Long Text

---
+++ Migrations/20200130144237_CardImageLength.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PinyinCardApi.Migrations
+{
+    public partial class CardImageLength : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image",
+                table: "card",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(255) CHARACTER SET utf8mb4",
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "image",
+                table: "card",
+                type: "varchar(255) CHARACTER SET utf8mb4",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
