a3b7a565d5025c8aa7b9bc55ed1ab3dffef09153
Piero Giusti <pierophp@gmail.com>
2023-11-03 11:30:43 -0300

    Category

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -23,39 +23,39 @@ public class Category : BaseEntity
     [Key, Column("id", Order = 0)]
     public int Id { get; set; }
 
-    // [StringLength(255, ErrorMessage = "NameEn can't be longer than 255 characters")]
-    // [Column("name_en")]
-    // public string NameEn { get; set; }
+    [StringLength(255, ErrorMessage = "NameEn can't be longer than 255 characters")]
+    [Column("name_en")]
+    public string NameEn { get; set; }
 
-    // [StringLength(255, ErrorMessage = "NamePt can't be longer than 255 characters")]
-    // [Column("name_pt")]
-    // public string NamePt { get; set; }
+    [StringLength(255, ErrorMessage = "NamePt can't be longer than 255 characters")]
+    [Column("name_pt")]
+    public string NamePt { get; set; }
 
-    // [StringLength(255, ErrorMessage = "NameCht can't be longer than 255 characters")]
-    // [Column("name_cht")]
-    // public string NameCht { get; set; }
+    [StringLength(255, ErrorMessage = "NameCht can't be longer than 255 characters")]
+    [Column("name_cht")]
+    public string NameCht { get; set; }
 
-    // [StringLength(255, ErrorMessage = "NameChs can't be longer than 255 characters")]
-    // [Column("name_chs")]
-    // public string NameChs { get; set; }
+    [StringLength(255, ErrorMessage = "NameChs can't be longer than 255 characters")]
+    [Column("name_chs")]
+    public string NameChs { get; set; }
 
-    // [StringLength(255, ErrorMessage = "NameIt can't be longer than 255 characters")]
-    // [Column("name_it")]
-    // public string NameIt { get; set; }
+    [StringLength(255, ErrorMessage = "NameIt can't be longer than 255 characters")]
+    [Column("name_it")]
+    public string NameIt { get; set; }
 
-    // [StringLength(255, ErrorMessage = "NameFr can't be longer than 255 characters")]
-    // [Column("name_fr")]
-    // public string NameFr { get; set; }
+    [StringLength(255, ErrorMessage = "NameFr can't be longer than 255 characters")]
+    [Column("name_fr")]
+    public string NameFr { get; set; }
 
-    // [StringLength(255, ErrorMessage = "NameFr can't be longer than 255 characters")]
-    // [Column("name_de")]
-    // public string NameDe { get; set; }
+    [StringLength(255, ErrorMessage = "NameFr can't be longer than 255 characters")]
+    [Column("name_de")]
+    public string NameDe { get; set; }
 
-    // [Column("parent_category_id")]
-    // public int? ParentCategoryId { get; set; }
+    [Column("parent_category_id")]
+    public int? ParentCategoryId { get; set; }
 
-    // [Column("parent_category_id")]
-    // public Category? ParentCategory { get; set; }
+    [Column("parent_category_id")]
+    public Category? ParentCategory { get; set; }
 
     // , TypeName = "Json"
     [Column("props")]
